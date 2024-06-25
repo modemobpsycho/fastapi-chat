f500df9c6df01aed660d8dac1016072710ad9483
Vadim Taratuta <hawaiianwork@mail.ru>
2024-06-25 23:26:59 +0300

    jwt auth

--- backend/database.py
+++ backend/database.py
@@ -1,13 +1,28 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base, DeclarativeMeta
 from sqlalchemy.orm import sessionmaker
-from config import DB_URL
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from typing import AsyncGenerator
+from config import DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER
 
-SQLALCHEMY_DATABASE_URL = DB_URL
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{
+    DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-Base = declarative_base()
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
+
+
+async_session_maker = sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False)
+
+Base: DeclarativeMeta = declarative_base()
+
+
+async def create_db_and_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
